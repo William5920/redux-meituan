42a42826cefdb4029e087967835132c00affb901
unknown <1101044179@qq.com>
2024-08-08 20:08:11 +0800

    删除多余文件，完成获取食品列表相关redux逻辑编写

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchFoodsList } from './store/modules/foodsReducer';
+import { useEffect } from 'react';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchFoodsList());
+  }, [dispatch]);
+
+  const { foodsList } = useSelector(state => state.foods);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>react app 根组件</h1>
+      <ul>
+        { foodsList.map(item => <li key={item.name}>{item.name}</li>) }
+      </ul>
     </div>
   );
 }
